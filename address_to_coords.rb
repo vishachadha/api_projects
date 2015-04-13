caf5668e6cb56fa3c8dc511f2c9ccc782503059d
vishachadha <Visha@dhcp-10-101-96-97.wireless.northwestern.private>
2015-04-13 13:11:21 -0500

    Finished address_to_coords!

--- address_to_coords.rb
+++ address_to_coords.rb
@@ -15,23 +15,24 @@ street_address = gets.chomp
 # Replaces spaces and other URL-illegal characters in the string
 url_safe_street_address = URI.encode(street_address)
 puts "test: Safe street address- #{url_safe_street_address}"
-#puts "#{url_safe_street_address.class}"
+puts "#{url_safe_street_address.class}"
 
 # Your code goes below. Hints:
 
-url_of_data_we_want = url_safe_street_address
+url_of_data_we_want = "http://maps.googleapis.com/maps/api/geocode/json?address=#{url_safe_street_address}"
+puts "test: url of data we want is #{url_of_data_we_want}"
 raw_data = open(url_of_data_we_want).read
 puts "test: printing raw_data #{raw_data}"
 parsed_data = JSON.parse(raw_data)
 
-# # ...
+# # # ...
 
-# # Let's store the latitude in a variable called 'the_latitude',
-# #   and the longitude in a variable called 'the_longitude'.
+# # # Let's store the latitude in a variable called 'the_latitude',
+# # #   and the longitude in a variable called 'the_longitude'.
 
 the_latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
 the_longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
-# # Ultimately, we want the following line to work when uncommented:
+# # # Ultimately, we want the following line to work when uncommented:
 
 puts "The latitude of #{street_address} is #{the_latitude} and the longitude is #{the_longitude}."
