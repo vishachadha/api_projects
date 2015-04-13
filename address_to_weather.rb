995a67f0055fe391b3c9955cbf714c67c53c057a
vishachadha <Visha@dhcp-10-101-96-97.wireless.northwestern.private>
2015-04-13 13:49:06 -0500

    Finished parsing weather data as required.

--- address_to_weather.rb
+++ address_to_weather.rb
@@ -14,9 +14,18 @@ street_address = gets.chomp
 url_safe_street_address = URI.encode(street_address)
 
 # Your code goes below.
+url_of_data_we_want = "https://api.forecast.io/forecast/f3fd7a235149bf9d4eb20dead0b3a9eb/#{url_safe_street_address}"
+#puts "test: printing url_data_we want- #{url_of_data_we_want}"
 
+raw_data = open(url_of_data_we_want).read
+#puts "test: printing raw_data #{raw_data}"
+parsed_data = JSON.parse(raw_data)
+
+the_temperature = parsed_data["currently"]["temperature"]
+the_hour_outlook = parsed_data["hourly"]["summary"]
+the_day_outlook = parsed_data["daily"]["summary"]
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The current temperature at #{street_address} is #{the_temperature} degrees."
-# puts "The outlook for the next hour is: #{the_hour_outlook}"
-# puts "The outlook for the next day is: #{the_day_outlook}"
+puts "The current temperature at #{street_address} is #{the_temperature} degrees."
+puts "The outlook for the next hour is: #{the_hour_outlook}"
+puts "The outlook for the next day is: #{the_day_outlook}"
